067f7206ff3a94c8cad09312033f959888bf4aa2
Renato Delgado <a20190271@pucp.edu.pe>
2024-05-03 20:33:22 -0500

    mi primer commit

---
+++ src/main/java/com/example/clase6_classwork/Clase6ClassworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.clase6_classwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clase6ClassworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Clase6ClassworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/clase6_classwork/Clase6ClassworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clase6_classwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clase6ClassworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
